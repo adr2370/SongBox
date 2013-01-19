7645fa4d5b4aa23a9be5a932a1a5ae2fdb62fcd8
Shahid Chohan <schohan.15@ucla.edu>
2013-01-18 18:21:28 -0800

    initial template update

---
+++ index.php
@@ -0,0 +1,132 @@
+<?php
+	require "Services/Twilio.php"; // for Twilio REST API
+
+	session_start(); // start session for Twilio SMS response 
+
+	$AccountSid = "AC015bae4a78bec6d2699d094ea378057b";
+    $AuthToken = "bcb0ad9983482a41ecc92443ab210dfa";
+	$client = new Services_Twilio($AccountSid, $AuthToken); // instantiate Twilio client
+	
+	// Twilio API Query 
+	$counter = $_SESSION['counter']; // sets up a session / conversation (Twilio saves info for 4 hours...ish)
+	$body = $_REQUEST['Body']; // gets the body of the message received
+	$number = $_REQUEST['From']; // gets the sender of the message received
+	$isAdmin = false;
+	if($number=="17145857755")
+	{
+		$isAdmin = true;
+	}
+	
+	//get list of rooms
+
+	//join room
+
+	//initiates counter to reflectnew session
+	if(!strlen($counter))
+	{
+		$counter = 0;
+	}
+
+	//new session
+	if($counter == 0)
+	{
+		$text = "Text back any of the following commands.\n".
+				"'add <SONG NAME>'\n".
+				"'info (to get song information)'\n".
+				"'queue (to see the current playlist queue'\n".
+				"'help' (to get a list of commands)\n".
+				"OR just text us how you feel about the song\n";
+		if($isAdmin)
+		{
+			$text = "ADMIN OPTIONS:\n".
+			"'skip'\n".
+			"'+' (to increase volume)\n".
+			"'-' (to decrease volume)\n" . $text;		
+		}
+	}
+	//handle adding new songs
+	else if(strtolower(substr($body, 0, 3))=="add")
+	{
+		//strip out important stuff from string
+		$song = "";
+		//if song matches, add to queue
+
+		//send song request to backend
+		$text = $song . " has been added to the queue! What do you think about the current song?";		
+	}
+	//get info
+	else if(strtolower(substr($body, 0, 8))=="info")
+	{
+		$text = getCurrentSongInfo();
+	}
+	//get queue
+	else if(strtolower(substr($body, 0, 6))=="queue")
+	{
+		$text = getQueue();	
+	}
+	else if(strtolower(substr($body, 0, 6))=="help")
+	{
+		$text = "Text back any of the following commands.\n".
+				"'add <SONG NAME>'\n".
+				"'info (to get song information)'\n".
+				"'queue (to see the current playlist queue'\n".
+				"'help' (to get a list of commands)\n".
+				"OR just text us how you feel about the song\n";
+		if($isAdmin)
+		{
+			$text = "ADMIN OPTIONS:\n".
+			"'skip'\n".
+			"'+' (to increase volume)\n".
+			"'-' (to decrease volume)\n" . $text;		
+		}		
+	}
+
+	
+	//ADMIN PANEL
+	//volume control ONLY FOR OWNER OF THE ROOM
+	else if(strtolower(substr($body, 0, 1))=="++" && isAdmin)
+
+	else if(strtolower(substr($body, 0, 1))=="--" && isAdmin)
+
+	else if(strtolower(substr($body, 0, 1))=="skip" && isAdmin)
+	{
+		skipCurrentSong()
+	}
+		
+
+	else
+	{
+		//handle up/downvoting song currently playing
+
+		//getCurrentSong
+		
+		//strip up/vote from song
+
+		//send text info to backend for sentiment analysis and voting		
+		$sentiment = getVoteResponse()
+		if($sentiment == -1)
+		{
+			$text = "That's too bad";					
+		}
+		else if($sentiment == 0)
+		{
+			$text = "Meh";					
+		}
+		else
+		{
+			$text = "Awesome";					
+		}
+		
+	}
+
+	//save session for future
+	$_SESSION['counter'] = $counter;
+
+
+	//sends text to user
+	$sms = $client->account->sms_messages->create("949-391-4022",$number, $text);
+	
+	
+	
+	
+?>
