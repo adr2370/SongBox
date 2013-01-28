d58ba276026a2f0d6c207d016a94a49023292945
Alexander Ramirez <adr2370@gmail.com>
2013-01-27 23:46:30 -0800

    Added in removing id from songs

--- js/room.js
+++ js/room.js
@@ -48,7 +48,7 @@ function startVideo() {
 		$("#visuals canvas").hide();
 		$("#"+id).remove();
 		songdb.child(id).remove();
-		//songs.splice(0,1);
+		removeA(songs, id);
 	});
 }
 
@@ -142,3 +142,14 @@ commentdb.on('child_added', function(snapshot, prevChildName) {
 	//snapshot.val() is each comment
 	$("#comments").prepend("<p style=\"font-size:30px;line-height:normal;\">" + snapshot.val()+"<\/p>");
 });
+
+function removeA(arr) {
+    var what, a = arguments, L = a.length, ax;
+    while (L > 1 && arr.length) {
+        what = a[--L];
+        while ((ax= arr.indexOf(what)) !== -1) {
+            arr.splice(ax, 1);
+        }
+    }
+    return arr;
+}
