50e554873209f91466043ef135cabf64de708a63
Alexander Ramirez <adr2370@gmail.com>
2013-02-04 12:39:44 -0800

    Fixed rating when no video bug

--- twilioStuff/getSentiment.php
+++ twilioStuff/getSentiment.php
@@ -5,16 +5,19 @@ $comment = $_GET['text'];
 $lymbix = new Lymbix("e46e988ca39ccf4b22287a6927bca3846985cf4d");
 $response = ($lymbix->Tonalize($comment));
 $data = $response->article_sentiment->sentiment;
-patchFirebase('comments',$comment,'POST');
-$rating=intval(file_get_contents("https://adr2370.firebaseio.com/current/rating/.json"));
-if($data=="Positive") {
-	$rating++;
-} else if($data=="Negative") {
-	$rating--;
+$rating=file_get_contents("https://adr2370.firebaseio.com/current/rating/.json");
+if($rating!="null") {
+	$rating=intval($rating);
+	patchFirebase('comments',$comment,'POST');
+	if($data=="Positive") {
+		$rating++;
+	} else if($data=="Negative") {
+		$rating--;
+	}
+	patchFirebase('current/rating',$rating,"PUT");
+	if($rating<-5) {
+		patchFirebase("current/skip","1","PUT");
+	}
+	echo $data;
 }
-patchFirebase('current/rating',$rating,"PUT");
-if($rating<-5) {
-	patchFirebase("current/skip","1","PUT");
-}
-echo $data;
 ?>
